2c031a78c95e5e8837b96860ab4d13f21c9b66ab
Dhanushka Rathnayaka <pdhanushka856@gmail.com>
2023-12-25 11:49:58 +0530

    Home Page Responsive

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,6 @@
 const moonIcon = document.getElementById("moon-icon");
 const body = document.getElementsByTagName("body");
 
-// const savedClassState = localStorage.getItem("classState");
-
-// moonIcon.onclick = function () {
-//   document.body.classList.toggle("dark-mode");
-//   if (document.body.classList.contains("dark-mode")) {
-//     moonIcon.src = "Img/pngwing.com (8).png";
-//   } else {
-//     moonIcon.src = "Img/2682847_eclipse_forecast_moon_night_space_icon.png";
-//   }
-// };
-
 document.addEventListener("DOMContentLoaded", function () {
   // Retrieve the class state from localStorage
   const savedClassState = localStorage.getItem("classState");
@@ -59,3 +48,73 @@ window.addEventListener("scroll", handleStickyNav);
 
 // Initial setup
 handleStickyNav();
+
+const cardAs = document.querySelectorAll(".card-a");
+const cardBs = document.querySelectorAll(".card-b");
+const cardCs = document.querySelectorAll(".card-c");
+const cardDs = document.querySelectorAll(".card-d");
+const img01s = document.querySelectorAll(".img01");
+const img02s = document.querySelectorAll(".img02");
+const img03s = document.querySelectorAll(".img03");
+const img04s = document.querySelectorAll(".img04");
+
+const options = {
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    const intersecting = entry.isIntersecting;
+    if (intersecting) {
+      entry.target.classList.add("transform0");
+      entry.target.classList.add("see");
+
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove("transform0");
+      entry.target.classList.remove("see");
+    }
+  });
+}, options);
+
+cardAs.forEach((cardA) => {
+  observer.observe(cardA);
+});
+cardBs.forEach((cardB) => {
+  observer.observe(cardB);
+});
+cardCs.forEach((cardC) => {
+  observer.observe(cardC);
+});
+cardDs.forEach((cardD) => {
+  observer.observe(cardD);
+});
+img01s.forEach((img01) => {
+  observer.observe(img01);
+});
+img02s.forEach((img02) => {
+  observer.observe(img02);
+});
+img03s.forEach((img03) => {
+  observer.observe(img03);
+});
+img04s.forEach((img04) => {
+  observer.observe(img04);
+});
+
+// nav bar
+
+const openMenu = document.getElementById("open-icon");
+const closeMenu = document.getElementById("close-icon");
+const navMenu = document.getElementById("nav-menu");
+
+openMenu.addEventListener("click", function () {
+  navMenu.style.display = "flex";
+  openMenu.style.display = "none";
+  closeMenu.style.display = "flex";
+});
+closeMenu.addEventListener("click", function () {
+  navMenu.style.display = "none";
+  openMenu.style.display = "flex";
+  closeMenu.style.display = "none";
+});
